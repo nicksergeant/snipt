4204fbbc1a79e3a00106b0b395c272650b89035f
Nick Sergeant <nick@nicksergeant.com>
2011-10-23 23:13:37 -0400

    Testing fab

--- snipt/fabfile.py
+++ snipt/fabfile.py
@@ -1,4 +1,8 @@
+import os
+
 from fabric.api import local
 
 def deploy():
-    print 'hi'
+    BASE_PATH = os.path.dirname(__file__)
+    local('lessc %s/media/css/style.less %s/media/css/style.css' % (BASE_PATH, BASE_PATH))
+    local('coffee -c %s/media/js/script.coffee' % (BASE_PATH))
