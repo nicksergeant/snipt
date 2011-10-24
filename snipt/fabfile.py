32f2aab7cdcdb4d5d2fe9c0444e6bff336e4a13c
Nick Sergeant <nick@nicksergeant.com>
2011-10-24 00:18:56 -0400

    Autocommit by [fab staticfiles]

--- snipt/fabfile.py
+++ snipt/fabfile.py
@@ -9,13 +9,13 @@ def staticfiles():
     local('sed -i -e \'s/\/media\//https:\/\/dn2p0mzo970os.cloudfront.net\//g\' %s/media/css/style.css' % BASE_PATH)
     local('rm %s/media/css/style.css-e' % BASE_PATH)
     local('cat %s/media/css/*.css > %s/media/cache/snipt.css' % (BASE_PATH, BASE_PATH))
-    local('cat %s/media/js/*.js > %s/media/cache/snipt.js' % (BASE_PATH, BASE_PATH))
+    local('cat %s/media/js/jquery.js %s/media/js/jquery.*.js %s/media/js/script.js > %s/media/cache/snipt.js' % (BASE_PATH, BASE_PATH, BASE_PATH, BASE_PATH))
     try:
         local('hg commit -m "Autocommit by [fab staticfiles]"')
         local('hg push')
     except:
         pass
-    local('%s/manage.py collectstatic' % BASE_PATH)
+    #local('%s/manage.py collectstatic' % BASE_PATH)
 
 def deployall():
     staticfiles()
