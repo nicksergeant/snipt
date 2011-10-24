88dfc45ac7e85b4a7f4d82c3caed8a2df9c0b8bf
Nick Sergeant <nick@nicksergeant.com>
2011-10-23 23:16:51 -0400

    More fab

--- snipt/fabfile.py
+++ snipt/fabfile.py
@@ -2,7 +2,16 @@ import os
 
 from fabric.api import local
 
-def deploy():
+def staticfiles():
     BASE_PATH = os.path.dirname(__file__)
     local('lessc %s/media/css/style.less %s/media/css/style.css' % (BASE_PATH, BASE_PATH))
-    local('coffee -c %s/media/js/script.coffee' % (BASE_PATH))
+    local('coffee -c %s/media/js/script.coffee' % BASE_PATH)
+    local('%s/manage.py collectstatic' % BASE_PATH)
+
+def deployall()
+    staticfiles()
+    deployapp()
+
+def deployapp()
+    local('hgp')
+    local('hg push-heroku')
