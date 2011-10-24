1fef6fb13bd8e79d04577c436b6588745d15c21a
Nick Sergeant <nick@nicksergeant.com>
2011-10-23 23:35:53 -0400

    Autocommit by [fab staticfiles]

--- snipt/fabfile.py
+++ snipt/fabfile.py
@@ -6,12 +6,14 @@ def staticfiles():
     BASE_PATH = os.path.dirname(__file__)
     local('lessc %s/media/css/style.less %s/media/css/style.css' % (BASE_PATH, BASE_PATH))
     local('coffee -c %s/media/js/script.coffee' % BASE_PATH)
+    local('sed -i -e \'s/\/media\//https:\/\/dn2p0mzo970os.cloudfront.net\//g\' %s/media/css/style.css' % BASE_PATH)
+    local('rm %s/media/css/style.css-e' % BASE_PATH)
     try:
         local('hg commit -m "Autocommit by [fab staticfiles]"')
         local('hg push')
     except:
         pass
-    local('%s/manage.py collectstatic' % BASE_PATH)
+    #local('%s/manage.py collectstatic' % BASE_PATH)
 
 def deployall():
     staticfiles()
