37af280223eef67ee8903acfb8d801d42b5f03bd
Nick Sergeant <nick@nicksergeant.com>
2011-10-23 23:46:32 -0400

    Fix

--- snipt/fabfile.py
+++ snipt/fabfile.py
@@ -8,8 +8,8 @@ def staticfiles():
     local('coffee -c %s/media/js/script.coffee' % BASE_PATH)
     local('sed -i -e \'s/\/media\//https:\/\/dn2p0mzo970os.cloudfront.net\//g\' %s/media/css/style.css' % BASE_PATH)
     local('rm %s/media/css/style.css-e' % BASE_PATH)
-    local('cat %s/media/css/* > snipt.css' % BASE_PATH)
-    local('cat %s/media/js/* > snipt.js' % BASE_PATH)
+    local('cat %s/media/css/* > %s/media/cache/snipt.css' % (BASE_PATH, BASE_PATH))
+    local('cat %s/media/js/* > %s/media/cache/snipt.js' % (BASE_PATH, BASE_PATH))
     try:
         local('hg commit -m "Autocommit by [fab staticfiles]"')
         local('hg push')
