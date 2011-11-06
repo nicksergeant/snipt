9b02413c39d2dfe11419191de54f9cbb2112f9b7
Nick Sergeant <nick@nicksergeant.com>
2011-11-06 18:31:32 -0500

    comments

--- snipt/media/cache/snipt.js
+++ snipt/media/cache/snipt.js
@@ -131,12 +131,15 @@ jQuery(function($) {
 
         initialize: function(opts) {
 
+            // Site variables
             $search_query = $('input#search-query', this.el);
             $snipts = $('section#snipts article.snipt', this.el);
 
+            // Init functions
             this.keyboardShortcuts();
             this.inFieldLabels();
 
+            // Init snipts
             if ($snipts.length) {
                 SniptListView = Snipt.Views.SniptListView;
                 Snipts = new SniptListView({ 'snipts': $snipts });
