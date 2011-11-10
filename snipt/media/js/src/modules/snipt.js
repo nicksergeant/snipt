482f2cb06cfd6d4e21b74e90dbb070c87f34cc7a
Nick Sergeant <nick@nicksergeant.com>
2011-11-10 16:04:16 -0500

    Copy func

--- snipt/media/js/src/modules/snipt.js
+++ snipt/media/js/src/modules/snipt.js
@@ -7,10 +7,17 @@
         initialize: function() {
             this.model = new Snipt.SniptModel();
             this.model.view = this;
+
             this.$el = $(this.el);
-            this.$container = $('div.container', this.$el);
             this.$aside = $('aside', this.$el);
-            this.$expand_button = $('a.expand', this.$el);
+            this.$container = $('div.container', this.$el);
+            this.$copy_button = $('a.copy', this.$aside);
+            this.$copy_do = $('span.do', this.$copy_button);
+            this.$copy_done = $('span.done', this.$copy_button);
+            this.$expand_button = $('a.expand', this.$aside);
+            this.$raw = $('div.raw', this.$container);
+
+            this.setupCopy();
         },
         events: {
             'click a.expand': 'expand'
@@ -19,6 +26,23 @@
             this.$container.toggleClass('expanded', 100);
             this.$aside.toggleClass('expanded');
             return false;
+        },
+        setupCopy: function() {
+            var copy_do = this.$copy_do;
+            var copy_done = this.$copy_done;
+
+            this.$copy_button.zclip({
+                afterCopy: function() {
+                    copy_do.hide();
+                    copy_done.fadeIn(500);
+                    setTimeout(function() {
+                        copy_done.hide();
+                        copy_do.fadeIn(500);
+                    }, 1500);
+                },
+                copy: this.$raw.text(),
+                path: '/media/swfs/zero-clipboard.swf'
+            });
         }
     });
 
