91b6990dd803211313bfa1911dc5f3893843b72b
Nick Sergeant <nick@nicksergeant.com>
2011-11-10 16:22:43 -0500

    Troubles

--- snipt/urls.py
+++ snipt/urls.py
@@ -24,4 +24,8 @@ urlpatterns = patterns('',
     url(r'^api/', include(public_api.urls)),
 
     url(r'^', include('snipts.urls')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+)
