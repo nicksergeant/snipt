1db314ebb5e5b29981dcaf6f052b97c485a8885a
Nick Sergeant <nick@nicksergeant.com>
2012-01-15 19:50:14 -0500

    moar

--- snipt/media/cache/snipt.js
+++ snipt/media/cache/snipt.js
@@ -963,30 +963,40 @@ jQuery(function($) {
 (function(Snipt) {
 
     Snipt.SniptModel = Backbone.Model.extend({
+        url: '',
+        title: ''
     });
     SniptView = Backbone.View.extend({
+
         initialize: function() {
-            this.model = new Snipt.SniptModel();
+            this.$el = $(this.el);
+            this.$h1 = $('header h1 a', this.$el);
+
+            this.model = new Snipt.SniptModel({
+                title: this.$h1.text(),
+                url: this.$h1.attr('href')
+            });
             this.model.view = this;
 
-            this.$el = $(this.el);
             this.$aside = $('aside', this.$el);
             this.$container = $('div.container', this.$el);
             this.$raw = $('div.raw', this.$container);
             this.$tags = $('section.tags ul', this.$aside);
         },
         events: {
-            'click a.copy':   'copy',
-            'click a.expand': 'expand',
-            'detail':         'detail',
-            'next':           'next',
-            'prev':           'prev',
-            'select':         'select'
+            'click a.copy':     'copy',
+            'click a.expand':   'expand',
+            'click .container': 'clickSelect',
+            'copy':             'copy',
+            'detail':           'detail',
+            'expand':           'expand',
+            'next':             'next',
+            'prev':             'prev',
+            'select':           'select'
         },
-        expand: function() {
-            this.$container.toggleClass('expanded', 100);
-            this.$tags.toggleClass('expanded');
-            return false;
+
+        clickSelect: function() {
+            this.select(true);
         },
         copy: function() {
             var cmd;
@@ -1000,25 +1010,13 @@ jQuery(function($) {
             window.prompt('Text is selected. To copy: press ' + cmd + '+C then <Enter>', this.$raw.text());
             return false;
         },
-        select: function() {
-
-            $('article.selected', SniptList.$el).removeClass('selected');
-            this.$el.addClass('selected');
-
-            if (SniptList.$snipts.index(this.$el) === 0) {
-                $('html, body').animate({
-                    scrollTop: 0
-                }, 0);
-            } else {
-                $('html, body').animate({
-                    scrollTop: this.$el.offset().top - 50
-                }, 0);
-            }
-
-            window.$selected = this.$el;
-        },
         detail: function() {
-            console.log('Going to detail view for ' + this.$el.find('h1').text());
+            window.location = this.model.get('url');
+        },
+        expand: function() {
+            this.$container.toggleClass('expanded', 100);
+            this.$tags.toggleClass('expanded');
+            return false;
         },
         next: function() {
             nextSnipt = this.$el.next('article.snipt');
@@ -1031,6 +1029,25 @@ jQuery(function($) {
             if (prevSnipt.length) {
                 return prevSnipt.trigger('select');
             }
+        },
+        select: function(fromClick) {
+
+            $('article.selected', SniptList.$el).removeClass('selected');
+            this.$el.addClass('selected');
+
+            if (fromClick !== true) {
+                if (SniptList.$snipts.index(this.$el) === 0) {
+                    $('html, body').animate({
+                        scrollTop: 0
+                    }, 0);
+                } else {
+                    $('html, body').animate({
+                        scrollTop: this.$el.offset().top - 50
+                    }, 0);
+                }
+            }
+
+            window.$selected = this.$el;
         }
     });
     SniptListView = Backbone.View.extend({
@@ -1053,31 +1070,46 @@ jQuery(function($) {
             $snipts = this.$snipts;
             $el = this.$el;
 
-            $(document).bind('keydown', 'j', function(e) {
+            $(document).bind('keydown', 'c', function() {
+                if ($selected) {
+                    $selected.trigger('copy');
+                }
+            });
+            $(document).bind('keydown', 'e', function() {
+                if ($selected) {
+                    if ($selected.hasClass('expandable')) {
+                        $selected.trigger('expand');
+                    }
+                }
+            });
+            $(document).bind('keydown', 'esc', function() {
+                if ($selected) {
+                    $selected.removeClass('selected');
+                    $selected = false;
+                }
+            });
+            $(document).bind('keydown', 'j', function() {
                 if (!$selected) {
                     $snipts.eq(0).trigger('select');
                 } else {
                     $selected.trigger('next');
                 }
             });
-            $(document).bind('keydown', 'k', function(e) {
+            $(document).bind('keydown', 'k', function() {
                 if (!$selected) {
                     $snipts.eq(0).trigger('select');
                 } else {
                     $selected.trigger('prev');
                 }
             });
-            $(document).bind('keydown', 'return', function(e) {
-                if (!$selected) {
-                    return false;
-                }
-                $selected.trigger('detail');
-                return false;
-            });
-            $(document).bind('keydown', 'esc', function(e) {
+            $(document).bind('keydown', 'o', function(e) {
                 if ($selected) {
-                    $selected.removeClass('selected');
-                    $selected = false;
+                    $selected.trigger('detail');
+                }
+            });
+            $(document).bind('keydown', 'return', function(e) {
+                if ($selected) {
+                    $selected.trigger('detail');
                 }
             });
         }
