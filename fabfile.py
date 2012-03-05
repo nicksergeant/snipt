b62ae33d7bc4812c8864f2b8a188f7e56d1e9c4e
Nick Sergeant <nick@nicksergeant.com>
2012-03-05 13:06:17 -0500

    Fab

--- fabfile.py
+++ fabfile.py
@@ -7,8 +7,6 @@ def staticfiles():
 
     # CSS
     local('sass %s/media/css/style.scss %s/media/css/style.css' % (BASE_PATH, BASE_PATH))
-    #local('sed -i -e \'s/\/media\//https:\/\/snipt.s3.amazonaws.com\//g\' %s/media/css/style.css' % BASE_PATH)
-    #local('rm %s/media/css/style.css-e' % BASE_PATH)
     css = [
         '%s/media/css/bootstrap.css' % BASE_PATH,
         '%s/media/css/style.css' % BASE_PATH,
@@ -40,14 +38,7 @@ def deployapp(m):
         local('hg commit -m \'%s\'' % m)
     except:
         pass
-    try:
-        local('git add .')
-        local('git commit -m \'%s\'' % m)
-    except:
-        pass
     local('hg push')
-    #local('git push -f heroku')
-    #local('heroku restart')
 
 def deploy(m):
     staticfiles()
@@ -55,5 +46,3 @@ def deploy(m):
 
 def deployall(m):
     deploy(m)
-    #local('heroku run bin/python snipt/manage.py syncdb')
-    #local('heroku run bin/python snipt/manage.py migrate')
