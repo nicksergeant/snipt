2959328333b59f9f2cb903b976ed26d8c94b74e0
Nick Sergeant <nick@nicksergeant.com>
2012-03-05 16:57:18 -0500

    Go

--- fabfile.py
+++ fabfile.py
@@ -3,9 +3,22 @@ from fabric.api import cd, local, env, run
 
 env.hosts = ['nick@beta.snipt.net:39039']
 env.site_path = '/var/www/snipt'
+env.venv_path = '/home/nick/.virtualenvs/snipt'
 
-def deploy():
+def _python(cmd):
+    return env.venv_path.rstrip('/') + '/bin/python ' + cmd
+
+def deploy(m):
     local('python manage.py collectstatic --ignore grappelli --ignore admin --noinput')
+
+    try:
+        local('hg commit -m {}'.format(m))
+        local('hg push')
+    except:
+        pass
+
     with cd(env.site_path):
         run('hg pull -u')
-        run('python manage.py collectstatic --ignore grappelli --ignore admin --noinput')
+        run(_python('manage.py collectstatic --ignore grappelli --ignore admin --noinput'))
+
+
