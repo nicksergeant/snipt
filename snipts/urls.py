6a2169e9cbb73084c74bd1a1cd820e57f16832fa
Nick Sergeant <nick@nicksergeant.com>
2012-05-03 22:21:04 -0400

    Fix buggy view URL

--- snipts/urls.py
+++ snipts/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     url(r'^public/tag/(?P<tag_slug>[^/]+)/$',              views.list_public,   name='list-public-tag'),
     url(r'^embed/(?P<snipt_key>[^/]+)/$',                  views.embed,         name='embed'),
     url(r'^(?P<username_or_custom_slug>[^/]+)/$',          views.list_user,     name='list-user'),
-    url(r'^(?P<username>[^/]+)/tag/(?P<tag_slug>[^/]+)/$', views.list_user_tag, name='list-user-tag'),
+    url(r'^(?P<username>[^/]+)/tag/(?P<tag_slug>[^/]+)/$', views.list_user, name='list-user-tag'),
     url(r'^(?P<username>[^/]+)/(?P<snipt_slug>[^/]+)/$',   views.detail,        name='detail'),
 )
