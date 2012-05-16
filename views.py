7457bb440e5d0550dd66658300d81339c0266d47
Nick Sergeant <nick@nicksergeant.com>
2012-05-16 10:53:22 -0400

    Organize lexers a bit.

--- views.py
+++ views.py
@@ -4,4 +4,26 @@ from snipts.utils import get_lexers_list
 
 @ajax_request
 def lexers(request):
-    return {'objects': get_lexers_list()}
+    lexers = get_lexers_list()
+    objects = []
+
+    for l in lexers:
+
+        try:
+            filters = l[2]
+        except IndexError:
+            filters = []
+
+        try:
+            mimetypes = l[3]
+        except IndexError:
+            mimetypes = []
+
+        objects.append({
+            'name': l[0],
+            'lexers': l[1],
+            'filters': filters,
+            'mimetypes': mimetypes
+        })
+
+    return {'objects': objects}
