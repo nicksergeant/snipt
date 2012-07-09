931a5d70dc84e8b39d7f663c11196b26d9b1b68a
Nick Sergeant <nick@nicksergeant.com>
2012-07-08 23:44:47 -0400

    Some page rendering performance tweaks

--- media/js/compile-js.sh
+++ media/js/compile-js.sh
@@ -5,13 +5,11 @@ cat media/js/src/modules/site.js|jsmin > media/js/src/modules/site.min.js
 cat media/js/src/modules/snipt.js|jsmin > media/js/src/modules/snipt.min.js
 
 cat media/js/libs/underscore.js \
-    media/js/libs/jquery.js \
     media/js/libs/json2.js \
     media/js/libs/backbone.js \
     media/js/libs/bootstrap.js \
     media/js/plugins/jquery.hotkeys.js \
     media/js/plugins/jquery.infieldlabel.js \
-    media/js/plugins/jquery.ui.js \
     media/js/plugins/jquery.chosen.js \
     media/js/src/application.min.js \
     media/js/src/modules/site.min.js \
