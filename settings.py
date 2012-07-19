806c09495af916e0ed449737be2f427a0cf3eece
Nick Sergeant <nick@nicksergeant.com>
2012-07-18 20:22:20 -0400

    Disable caching because I don't know what I'm doing.

--- settings.py
+++ settings.py
@@ -212,11 +212,6 @@ ABSOLUTE_URL_OVERRIDES = {
 # Accounts
 AUTH_PROFILE_MODULE = 'accounts.UserProfile'
 
-# Caching
-CACHE_BACKEND = 'caching.backends.memcached://localhost:11211'
-CACHE_PREFIX = 'snipt:'
-CACHE_COUNT_TIMEOUT = 60
-
 # Search
 HAYSTACK_CONNECTIONS = {
     'default': {
