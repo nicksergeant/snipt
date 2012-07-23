e46e7b5ecef8e905771ce3e399af6427bd084eb1
Nick Sergeant <nick@nicksergeant.com>
2012-07-23 13:02:39 -0400

    Josh stuff

--- snipts/models.py
+++ snipts/models.py
@@ -107,6 +107,8 @@ class Snipt(models.Model):
         if self.blog_post:
             if self.user.id == 3:
                 return 'http://nicksergeant.com/{}/'.format(self.slug)
+            elif self.user.id == 10325:
+                return 'http://snipt.joshhudnall.com/{}/'.format(self.slug)
             else:
                 return 'https://{}.snipt.net/{}/'.format(self.user.username.replace('_', '-'), self.slug)
 
