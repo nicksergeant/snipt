bf21af8dcfaf3736ec4ad441dd86124f2d5eae6a
Nick Sergeant <nick@nicksergeant.com>
2012-07-25 22:51:32 -0400

    Elastic search. Fuck you, Solr.

--- settings.py
+++ settings.py
@@ -218,8 +218,9 @@ AUTH_PROFILE_MODULE = 'accounts.UserProfile'
 # Search
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-        'URL': 'http://127.0.0.1:8983/solr'
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
     },
 }
 
