ea8232ad2ecefae1b4fafe0eef21e48cc70f7594
Nick Sergeant <nick@nicksergeant.com>
2012-08-01 14:21:54 -0400

    Changing to support@snipt.net

--- settings.py
+++ settings.py
@@ -177,8 +177,8 @@ LOGGING = {
 }
 
 # Email
-DEFAULT_FROM_EMAIL = 'site@snipt.net'
-SERVER_EMAIL = 'site@snipt.net'
+DEFAULT_FROM_EMAIL = 'support@snipt.net'
+SERVER_EMAIL = 'support@snipt.net'
 EMAIL_BACKEND = 'postmark.django_backend.EmailBackend'
 POSTMARK_API_KEY = '608d3101-1706-4a96-819f-f2f36fe00fe0'
 
