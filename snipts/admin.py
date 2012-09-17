b461d8ee74bf49fd377ee626dbc3f5d215aef8b8
Nick Sergeant <nick@nicksergeant.com>
2012-09-17 13:36:06 -0400

    Admin and stats on Pro

--- snipts/admin.py
+++ snipts/admin.py
@@ -5,7 +5,7 @@ from snipts.models import Favorite, Snipt
 
 class SniptAdmin(admin.ModelAdmin):
     readonly_fields = ('user',)
-    list_display = ('title', 'slug', 'user', 'lexer', 'public', 'blog_post', 'created', 'modified', 'publish_date',)
+    list_display = ('title', 'slug', 'views', 'favs', 'user', 'lexer', 'public', 'blog_post', 'created', 'modified', 'publish_date',)
     list_filter = ('blog_post',)
     search_fields = ('title', 'slug', 'user__username', 'lexer', 'id', 'key',)
     ordering = ('-created',)
