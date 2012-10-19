4ded36f2f0ee4c72aa7064c9962fe75cbf3cb169
Andrew Stewart <andrew@averagestudios.com>
2012-10-18 21:21:14 -0700

    Relocating some stuff back to settings.py

--- settings.py
+++ settings.py
@@ -2,6 +2,25 @@
 
 import os
 
+BASE_PATH = os.path.dirname(__file__)
+
+ADMINS = (
+    ('Name', 'name@domain.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'snipt',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
 INTERNAL_IPS = ('127.0.0.1',)
 
 SITE_ID = 1
@@ -14,11 +33,76 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
+# Local time zone. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+#
+# On Unix systems, a value of None will cause Django to use the same timezone as
+# the operating system. On a Windows environment, this must be set to the same
+# as your system time zone
+TIME_ZONE = 'America/New_York'
+
+# Language code for the Django installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(BASE_PATH, 'media/uploads')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/uploads/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(BASE_PATH, 'static')
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_PATH, 'media'),
+)
+
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+SECRET_KEY = ''
+
+INSTALLED_APPS = (
+    'gunicorn',
+
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.humanize',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+
+    'debug_toolbar',
+    'django_bcrypt',
+    'haystack',
+    'markdown_deux',
+    'pagination',
+    'postmark',
+    'registration',
+    'south',
+    'taggit',
+    'tastypie',
+    'typogrify',
+
+    'accounts',
+    'blogs',
+    'snipts',
+    'utils',
 )
 
 # List of callables that know how to import templates from various sources.
@@ -77,6 +161,12 @@ LOGGING = {
             #'propagate': True,
         #},
     }
+}
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
+    },
 }
 
 # Account settings
