91cfaee3a3bb2f857f664238b0daf4741a6a6811
Andrew Stewart <andrew@averagestudios.com>
2012-10-18 19:50:12 -0700

    Moved a number of settings to local_settings.py

--- local_settings-template.py
+++ local_settings-template.py
@@ -1,6 +1,13 @@
+import os
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+BASE_PATH = os.path.dirname(__file__)
+
 ADMINS = (
     ('Name', 'name@domain.com'),
 )
+
 MANAGERS = ADMINS
 
 DATABASES = {
@@ -14,6 +21,72 @@ DATABASES = {
     }
 }
 
+INSTALLED_APPS = (
+    'gunicorn',
+
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.humanize',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+
+    'debug_toolbar',
+    'django_bcrypt',
+    'haystack',
+    'markdown_deux',
+    'pagination',
+    'postmark',
+    'registration',
+    'south',
+    'taggit',
+    'tastypie',
+    'typogrify',
+
+    'accounts',
+    'blogs',
+    'snipts',
+    'utils',
+)
+
+# Local time zone. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+#
+# On Unix systems, a value of None will cause Django to use the same timezone as
+# the operating system. On a Windows environment, this must be set to the same
+# as your system time zone
+TIME_ZONE = 'America/New_York'
+
+# Language code for the Django installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(BASE_PATH, 'media/uploads')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/uploads/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(BASE_PATH, 'static')
+if DEBUG:
+    STATIC_URL = '/media/'
+else:
+    STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_PATH, 'media'),
+)
+
 SECRET_KEY = ''
 
 DEFAULT_FROM_EMAIL = 'support@snipt.net'
@@ -21,6 +94,7 @@ SERVER_EMAIL = 'support@snipt.net'
 EMAIL_BACKEND = 'postmark.django_backend.EmailBackend'
 POSTMARK_API_KEY = ''
 
+# Virtualenv
 VIRTUALENV_PATH = ''
 
 AMAZON_API_KEY = ''
@@ -30,6 +104,35 @@ STRIPE_API_KEY = ''
 
 ENV_HOST = 'user@domain.com:22'
 
+# Bugsnag
 BUGSNAG = {
     "api_key": "",
+    "project_root": PROJECT_PATH,
 }
+
+# HTTPS
+if not DEBUG:
+    USE_HTTPS = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_DOMAIN = '.snipt.net'
+
+# Search
+if not DEBUG:
+    HAYSTACK_CONNECTIONS = {
+        'default': {
+            'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+            'URL': 'http://127.0.0.1:9200/',
+            'INDEX_NAME': 'haystack',
+        },
+    }
+else:
+    HAYSTACK_CONNECTIONS = {
+        'default': {
+            'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
+        },
+    }
+
+# Extensions
+if DEBUG:
+    INSTALLED_APPS += ('django_extensions',)
--- settings.py
+++ settings.py
@@ -1,45 +1,8 @@
 # Django settings for snipt project.
 
-import os, socket
-
-if (os.environ.get('SNIPT_ENV') == 'development'):
-    DEBUG = True
-else:
-    DEBUG = False
-
-TEMPLATE_DEBUG = DEBUG
-BASE_PATH = os.path.dirname(__file__)
-
-ADMINS = (
-    ('Name', 'name@domain.com'),
-)
-MANAGERS = ADMINS
+import os
 
 INTERNAL_IPS = ('127.0.0.1',)
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/New_York'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
@@ -51,39 +14,12 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(BASE_PATH, 'media/uploads')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/uploads/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(BASE_PATH, 'static')
-if DEBUG:
-    STATIC_URL = '/media/'
-else:
-    STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    os.path.join(BASE_PATH, 'media'),
-)
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -120,36 +56,6 @@ TEMPLATE_DIRS = (
     os.path.join(PROJECT_PATH, 'templates')
 )
 
-INSTALLED_APPS = (
-    'gunicorn',
-
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.humanize',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-
-    'debug_toolbar',
-    'django_bcrypt',
-    'haystack',
-    'markdown_deux',
-    'pagination',
-    'postmark',
-    'registration',
-    'south',
-    'taggit',
-    'tastypie',
-    'typogrify',
-
-    'accounts',
-    'blogs',
-    'snipts',
-    'utils',
-)
-
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
@@ -173,27 +79,11 @@ LOGGING = {
     }
 }
 
-# Email
-DEFAULT_FROM_EMAIL = 'support@snipt.net'
-SERVER_EMAIL = 'support@snipt.net'
-EMAIL_BACKEND = 'postmark.django_backend.EmailBackend'
-POSTMARK_API_KEY = ''
-
-# Virtualenv
-VIRTUALENV_PATH = ''
-
 # Account settings
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
 LOGOUT_URL = '/logout/'
 ACCOUNT_ACTIVATION_DAYS = 0
-
-# HTTPS
-if not DEBUG:
-    USE_HTTPS = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SESSION_COOKIE_DOMAIN = '.snipt.net'
 
 # Messages
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
@@ -206,22 +96,6 @@ ABSOLUTE_URL_OVERRIDES = {
 # Accounts
 AUTH_PROFILE_MODULE = 'accounts.UserProfile'
 
-# Search
-if not DEBUG:
-    HAYSTACK_CONNECTIONS = {
-        'default': {
-            'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-            'URL': 'http://127.0.0.1:9200/',
-            'INDEX_NAME': 'haystack',
-        },
-    }
-else:
-    HAYSTACK_CONNECTIONS = {
-        'default': {
-            'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
-        },
-    }
-
 # API
 TASTYPIE_CANNED_ERROR = "There was an error with your request. The site developers have a record of this error, please email api@snipt.net and we'll help you out."
 
@@ -230,10 +104,6 @@ BUGSNAG = {
     "api_key": "",
     "project_root": PROJECT_PATH,
 }
-
-# Extensions
-if DEBUG:
-    INSTALLED_APPS += ('django_extensions',)
 
 try:
     from local_settings import *
