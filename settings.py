6c1cfe164eaee1ab055336b4bad7b10bbda77929
Nick Sergeant <nick@nicksergeant.com>
2013-01-30 10:29:41 -0500

    Set the session cookie expiration to 6 months.

--- settings.py
+++ settings.py
@@ -140,6 +140,8 @@ TEMPLATE_DIRS = (
     os.path.join(PROJECT_PATH, 'templates')
 )
 
+SESSION_COOKIE_AGE = 14515200
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
