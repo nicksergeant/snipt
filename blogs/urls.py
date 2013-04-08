8ac70e56403aeaf517a1a186dca8b4da5d3ffc85
Nick Sergeant <nick@nicksergeant.com>
2013-04-08 10:06:49 -0400

    Scrizatch that.

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls import *
 
 urlpatterns = patterns('',
     url(r'^$', views.blog, name='blog'),
-    url(r'^login/$', lambda x: HttpResponseRedirect('https://snipt.net/login/')),
 )
