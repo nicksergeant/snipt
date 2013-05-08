ab86bef67a9f21f0de77c949d3ee07692fac52a4
Nick Sergeant <nick@nicksergeant.com>
2013-05-08 17:44:59 -0400

    Add intercom template tags.

---
+++ utils/templatetags/intercom.py
@@ -0,0 +1,10 @@
+from django import template
+from settings_local import INTERCOM_SECRET_KEY
+
+import hmac, hashlib
+
+register = template.Library()
+
+@register.filter
+def intercom_sha_256(user_id):
+    return hmac.new(INTERCOM_SECRET_KEY, user_id, digestmod=hashlib.sha256).hexdigest()
