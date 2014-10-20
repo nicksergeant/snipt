fe425f437dfd7ea744b3d85af79693b34dfe1c84
Nick Sergeant <nick@nicksergeant.com>
2014-10-20 11:18:08 -0400

    Working on Heroku integration.

--- settings.py
+++ settings.py
@@ -1,74 +1,45 @@
-# Django settings for snipt project.
-
 import dj_database_url, os
 
-DEBUG = True
-
-BASE_PATH = os.path.dirname(__file__)
-
-ADMINS = (
-    ('Name', 'name@domain.com'),
-)
-
-MANAGERS = ADMINS
-
-DATABASES['default'] =  dj_database_url.config()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ABSOLUTE_URL_OVERRIDES = { 'auth.user': lambda u: "/%s/" % u.username, }
+ACCOUNT_ACTIVATION_DAYS = 0
+ADMINS = (('Name', 'name@domain.com'),)
 ALLOWED_HOSTS = ['*']
-
+AUTH_PROFILE_MODULE = 'accounts.UserProfile'
+AUTHENTICATION_BACKENDS = ('utils.backends.EmailOrUsernameModelBackend',)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_PATH = os.path.dirname(__file__)
+DATABASES = { 'default': dj_database_url.config() }
+DEBUG = True
+HAYSTACK_CONNECTIONS = { 'default': { 'ENGINE': 'haystack.backends.simple_backend.SimpleEngine', }, }
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+INTERNAL_IPS = ('127.0.0.1',)
+LANGUAGE_CODE = 'en-us'
+LOGIN_REDIRECT_URL = '/login-redirect/'
+LOGIN_URL = '/login/'
+LOGOUT_URL = '/logout/'
+MANAGERS = ADMINS
+MEDIA_ROOT = os.path.join(BASE_PATH, 'media/uploads')
+MEDIA_URL = '/media/uploads/'
+MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+ROOT_URLCONF = 'urls'
+SECRET_KEY = 'changethis'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_AGE = 15801100
+SITE_ID = 1
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_FINDERS = ('django.contrib.staticfiles.finders.FileSystemFinder','django.contrib.staticfiles.finders.AppDirectoriesFinder',)
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'media'),
-    os.path.join(BASE_DIR, 'static'),
-)
-
-INTERNAL_IPS = ('127.0.0.1',)
-
-SITE_ID = 1
-
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = True
-
-# Local time zone. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-#
-# On Unix systems, a value of None will cause Django to use the same timezone as
-# the operating system. On a Windows environment, this must be set to the same
-# as your system time zone
+TASTYPIE_CANNED_ERROR = "There was an error with your request. The site developers have a record of this error, please email api@snipt.net and we'll help you out."
+TEMPLATE_DIRS = (os.path.join(PROJECT_PATH, 'templates'))
 TIME_ZONE = 'America/New_York'
+USE_I18N = True
+USE_L10N = True
 USE_TZ = True
-
-# Language code for the Django installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(BASE_PATH, 'media/uploads')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/uploads/'
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
-
-SECRET_KEY = 'changethis'
 
 INSTALLED_APPS = (
     'gunicorn',
-
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.humanize',
@@ -77,7 +48,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-
     'django_bcrypt',
     'haystack',
     'markdown_deux',
@@ -88,19 +58,31 @@ INSTALLED_APPS = (
     'taggit',
     'tastypie',
     'typogrify',
-
     'accounts',
     'blogs',
     'jobs',
     'snipts',
     'utils',
 )
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {},
+    'loggers': {}
+}
+MIDDLEWARE_CLASSES = (
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'pagination.middleware.PaginationMiddleware',
+    'blogs.middleware.BlogMiddleware',
 )
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
@@ -111,88 +93,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
     'django.contrib.messages.context_processors.messages',
 )
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'pagination.middleware.PaginationMiddleware',
-    'blogs.middleware.BlogMiddleware',
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
-
-AUTHENTICATION_BACKENDS = (
-    'utils.backends.EmailOrUsernameModelBackend',
-)
-
-ROOT_URLCONF = 'urls'
-
-PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
-
-TEMPLATE_DIRS = (
-    os.path.join(PROJECT_PATH, 'templates')
-)
-
-SESSION_COOKIE_AGE = 15801100
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        #'mail_admins': {
-            #'level': 'ERROR',
-            #'filters': ['require_debug_false'],
-            #'class': 'django.utils.log.AdminEmailHandler'
-        #}
-    },
-    'loggers': {
-        #'django.request': {
-            #'handlers': ['mail_admins'],
-            #'level': 'ERROR',
-            #'propagate': True,
-        #},
-    }
-}
-
-HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
-    },
-}
-HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
-
-# Account settings
-LOGIN_REDIRECT_URL = '/login-redirect/'
-LOGIN_URL = '/login/'
-LOGOUT_URL = '/logout/'
-ACCOUNT_ACTIVATION_DAYS = 0
-
-# Messages
-MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
-
-# User absolute URLs
-ABSOLUTE_URL_OVERRIDES = {
-    'auth.user': lambda u: "/%s/" % u.username,
-}
-
-# Accounts
-AUTH_PROFILE_MODULE = 'accounts.UserProfile'
-
-# API
-TASTYPIE_CANNED_ERROR = "There was an error with your request. The site developers have a record of this error, please email api@snipt.net and we'll help you out."
-
-try:
-    from settings_local import *
-except ImportError:
-    pass
