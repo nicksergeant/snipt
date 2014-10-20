7c23bed425b83d143d14d59c489075fc82905dae
Nick Sergeant <nick@nicksergeant.com>
2014-10-20 14:43:53 -0400

    Index name for Haystack.

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ if 'DATABASE_URL' in os.environ:
     HAYSTACK_CONNECTIONS = {
         'default': {
             'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-            'INDEX_NAME': 'snipt',
+            'INDEX_NAME': os.environ.get('BONSAI_INDEX', ''),
             'URL': os.environ.get('BONSAI_URL', ''),
         },
     }
