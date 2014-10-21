64237924b4c462718f46229a32dac339c0afd0ac
Nick Sergeant <nick@nicksergeant.com>
2014-10-20 22:02:04 -0400

    Static templates.

--- wsgi.py
+++ wsgi.py
@@ -11,5 +11,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
