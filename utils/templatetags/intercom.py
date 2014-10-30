2919f5c82b12601811e224c8e989cf3005505823
Nick Sergeant <nick@nicksergeant.com>
2014-10-30 12:17:47 -0400

    More Vagrant stuff.

--- utils/templatetags/intercom.py
+++ utils/templatetags/intercom.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django import template
 
 import hmac, hashlib, os
@@ -6,4 +7,4 @@ register = template.Library()
 
 @register.filter
 def intercom_sha_256(user_id):
-    return hmac.new(os.environ.get('INTERCOM_SECRET_KEY', ''), str(user_id), digestmod=hashlib.sha256).hexdigest()
+    return hmac.new(os.environ.get('INTERCOM_SECRET_KEY', settings.INTERCOM_SECRET_KEY), str(user_id), digestmod=hashlib.sha256).hexdigest()
