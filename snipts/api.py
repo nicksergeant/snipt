4a0a31b1995c95bdab0b960060a9267407d8744d
Nick Sergeant <nick@nicksergeant.com>
2014-11-23 15:54:48 -0500

    blog_post in public snipt resources.

--- snipts/api.py
+++ snipts/api.py
@@ -209,7 +209,7 @@ class PublicSniptResource(ModelResource):
         queryset = Snipt.objects.filter(public=True).order_by('-created')
         resource_name = 'snipt'
         fields = ['id', 'title', 'slug', 'lexer', 'code', 'description', 'line_count',
-                  'stylized', 'created', 'modified', 'publish_date',]
+                  'stylized', 'created', 'modified', 'publish_date', 'blog_post']
         include_absolute_url = True
         allowed_methods = ['get']
         filtering = { 'user': 'exact', }
