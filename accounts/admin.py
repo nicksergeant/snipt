307772165bd97de8dc3295ca66699a6e1f3f8f94
Nick Sergeant <nick@nicksergeant.com>
2015-07-22 14:02:32 -0400

    Show teams_beta_seen.

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from accounts.models import UserProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'is_pro', 'stripe_id', 'gittip_username',)
+    list_display = ('user', 'is_pro', 'stripe_id', 'gittip_username', 'teams_beta_seen')
     search_fields = ('user__username', 'gittip_username',)
 
 admin.site.register(UserProfile, UserProfileAdmin)
