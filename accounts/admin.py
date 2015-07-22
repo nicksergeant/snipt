34ae486fd543bbde827b087bb989fec709afd39b
Nick Sergeant <nick@nicksergeant.com>
2015-07-22 14:03:06 -0400

    Show teams_beta_seen.

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,7 @@ from accounts.models import UserProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'is_pro', 'stripe_id', 'gittip_username', 'teams_beta_seen')
+    list_filter = ['teams_beta_seen', 'teams_beta_applied']
     search_fields = ('user__username', 'gittip_username',)
 
 admin.site.register(UserProfile, UserProfileAdmin)
