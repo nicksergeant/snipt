f98aa4c34ca84cf9e30ceb73f71e4796d1e95c50
Nick Sergeant <nick@nicksergeant.com>
2015-11-05 21:10:25 -0500

    Redirect user to homepage.

--- utils/views.py
+++ utils/views.py
@@ -7,3 +7,6 @@ class SniptRegistrationView(RegistrationView):
     Custom registration view that uses our custom form.
     """
     form_class = SniptRegistrationForm
+
+    def get_success_url(self, request, user):
+        return '/{}/'.format(user.username)
