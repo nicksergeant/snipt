e4d56bf8614711f5dc7e2c2464fd69a136591b59
Nick Sergeant <nick@nicksergeant.com>
2016-03-21 12:35:21 -0400

    Make users recaptcha on signup.

--- utils/views.py
+++ utils/views.py
@@ -1,3 +1,7 @@
+import requests
+
+from django.conf import settings
+from django.http import HttpResponseBadRequest
 from registration.backends.default.views import RegistrationView
 from utils.forms import SniptRegistrationForm
 
@@ -7,6 +11,22 @@ class SniptRegistrationView(RegistrationView):
     Custom registration view that uses our custom form.
     """
     form_class = SniptRegistrationForm
+
+    def dispatch(self, request, *args, **kwargs):
+
+        if request.method == 'POST':
+            payload = {
+                'secret': settings.RECAPTCHA_SECRET,
+                'response': request.POST['g-recaptcha-response'],
+                'remoteip': request.META.get('REMOTE_ADDR')
+            }
+            r = requests.post('https://www.google.com/recaptcha/api/siteverify',
+                              data=payload)
+
+            if not r.json()['success']:
+                return HttpResponseBadRequest()
+
+        return super(RegistrationView, self).dispatch(request, *args, **kwargs)
 
     def get_success_url(self, request, user):
         return '/{}/'.format(user.username)
