932e2398335e03eac425a3c6e746c2ddb2493a04
Nick Sergeant <nick@nicksergeant.com>
2016-11-01 14:00:16 -0400

    Need settings.

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,10 @@
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+import os
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
